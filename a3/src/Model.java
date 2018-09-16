100a7c86e2e951dad24245966c372f7eb40ba034
Andrew Cash <cashandrew41@gmail.com>
2018-09-16 15:28:37 -0500

    buggy collision code

--- a3/src/Model.java
+++ a3/src/Model.java
@@ -11,6 +11,7 @@ class Model
     int scrollPos;
     ArrayList<Brick> bricks;
     Mario mario;
+    boolean isCollision = false;
 
     Model()
     {
@@ -20,8 +21,40 @@ class Model
 
     public void update()
     {
-        mario.update(bricks);
+        mario.update();
     }
+
+    // returns true iff two rectangles overlap
+	void collision()
+	{
+        for (int i=0; i < bricks.size(); i++)
+		{
+            if(mario.right_side < bricks.get(i).x)
+    			isCollision = false;
+    		if(mario.left_side > bricks.get(i).w)
+    			isCollision = false;
+    		if(mario.bottom_side < bricks.get(i).y) // assumes bigger is downward
+    			isCollision = false;
+    		if(mario.top_side > bricks.get(i).h) // assumes bigger is downward
+    			isCollision = false;
+    		else
+            {
+                isCollision = true;
+
+            }
+		}
+
+        if (isCollision == false)
+        {
+            System.out.println("no collision...");
+        }
+
+        if (isCollision == true)
+        {
+            System.out.println("COLLISION!!");
+        }
+
+	}
 
     void addBrick(int x1, int y1, int x2, int y2)
     {
